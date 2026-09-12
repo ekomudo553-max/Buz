877487e182661867da7b9ce37b7a0e5050c81380
ekomudo553-max <ekomudo553@gmail.com>
2026-09-12 07:49:26 +0100

    Create layout.jsx

---
+++ app/admin/app/dashboard/app/layout.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "BizLink",
+  description: "Local Business Directory",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
